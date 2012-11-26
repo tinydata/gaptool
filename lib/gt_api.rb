c1b320308e9e69c2c8d273032bb00c8e72a682f0
Matt Bailey <m@mdb.io>
2012-11-26 15:29:03 -0800

    start of REST client

--- lib/gt_api.rb
+++ lib/gt_api.rb
@@ -1,6 +1,25 @@
-class Nodes
+require 'httparty'
+require 'awesome_print'
+require 'json'
+
+class GaptoolServer
   include HTTParty
-  base_uri $BASEURI
+  base_uri 'http://localhost:9393'
 
   def initialize(user, apikey)
-    @auth = { 
+    @auth = { 'X-GAPTOOL-USER' => user, 'X-GAPTOOL-KEY' => apikey}
+  end
+
+  def getonenode(role, environment, number)
+    options = {:headers => @auth}
+    JSON::parse self.class.get("/host/#{role}/#{environment}/#{number}", options)
+  end
+
+  def getenvroles(role, environment)
+    options = {:headers => @auth}
+    JSON::parse self.class.get("/hosts/#{role}/#{environment}", options)
+  end
+
+end
+
+
